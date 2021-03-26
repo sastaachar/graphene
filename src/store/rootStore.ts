f9e6201c53d2b43ef1dda6724816d2a7bd0336c4
sastaachar <justinjohnmathew@karunya.edu.in>
2021-03-26 23:06:40 +0530

    [👕 Linting] : Basic linting fixes

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -5,5 +5,6 @@ import { NodeManagerReducer } from './nodeManager/nodeManagerReducer';
 
 const rootReducer = combineReducers({ NodeManager: NodeManagerReducer });
 export type AppState = ReturnType<typeof rootReducer>;
+
 export const rootStore: Store = createStore(rootReducer, composeWithDevTools());
 export type AppDispatch = typeof rootStore.dispatch;
